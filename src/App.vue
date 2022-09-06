c24527411a2d5ebc1ae340b3f2d0fc4eabe11554
YANG <1696403434@qq.com>
2022-09-06 18:12:37 +0800

    feat:登录页完成布局

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,20 @@
 <template>
 <div >
-<van-button type="primary">主要按钮</van-button>
-<van-button type="info">信息按钮</van-button>
-<van-button type="default">默认按钮</van-button>
-<van-button type="warning">警告按钮</van-button>
-<van-button type="danger">危险按钮</van-button>
-<div class="box"></div>
+<router-view></router-view>
 </div>
 </template>
 
 <script>
 
 export default {
+  data() {
+    return {
 
+    }
+  }
 }
 </script>
 
 <style scoped>
-.box{
-  width: 2rem;
-  height: 2rem;
-  background-color: red;
-}
+
 </style>
