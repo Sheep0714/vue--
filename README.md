f53da85b6db3555f3fbf829160f1700264af7d2d
YANG <1696403434@qq.com>
2022-09-06 16:14:03 +0800

    feat:postcss配置完毕

--- README.md
+++
@@ -1,9 +0,0 @@
-git log 查看commit 记录
-git reset xxx--hard  回退 xxx为对应的commit 记录
-git status 查看修改
-git add . 提交到暂存区
-
-git commit -m '' 提交到git仓库
-feat:新增特性
-style:样式的修政
-fixed:debug
--- src/main.js
+++ src/main.js
@@ -2,11 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import Vant from 'vant'
+// 引入icon
+import './assets/fonts/iconfont.css'
+// 引入vant组件
+import 'vant/lib/index.css'
+// 引入rem适配
+import 'amfe-flexible'
 Vue.config.productionTip = false
-
+Vue.use(Vant)
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: (h) => h(App)
 }).$mount('#app')
